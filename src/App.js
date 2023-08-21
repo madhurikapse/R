2bd105c963bcfec1877ebd9a90a4b712d4f49993
madhurikapse <madhurikapse573@gmail.com>
2023-08-21 23:54:43 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer/MadhuriFoot";
+import RoutesFile from "./Components/RouterFile";
+import "./style.css";
+
+function App() {
+  return (
+    <>
+   
+    <RoutesFile/>
+    <Footer/>
+
+    </>
+  );
+}
+
+export default App;
